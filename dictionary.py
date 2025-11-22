2cb29462dd4ce0f3ba40311d6e90fb2db020d96c
shivam <shivam984861@gmail.com>
2025-11-22 19:34:52 +0530

    new changes

---
+++ dictionary.py
@@ -0,0 +1,13 @@
+dict = {
+
+    "name" : "shivam das",
+    "age" : 21,
+    "marks" : 99.0,
+    "is_adult" : True,
+    "subjects" : ["python", "C", "java"],
+    "topics" : ("dictionary", "sets"),
+ 
+}
+print(type(dict))
+print(len(dict))
+print(dict)
