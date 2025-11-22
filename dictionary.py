8b124c819ae5b1f873d54fe6744aaa68859f196d
shivam <shivam984861@gmail.com>
2025-11-22 20:10:45 +0530

    new changes

--- dictionary.py
+++ dictionary.py
@@ -7,6 +7,16 @@ dict = {
     "topics" : ("dictionary", "sets"),         # "KEY" is "STRING" and the "KEYVALUE" is "TUPLE"
 # in the dictionary the "keys" also can be written in the form of str(), int(), boolean() and tuple()
     }
+
+print(dict["name"],dict["age"],dict["subjects"])
+# if we want to print any particular value in the dictionary we can use   "print(dict["key"])"
+
+dict["name"] = "shreya",
+# if we want to replace any value we can use this   "dict["name"] = 'new value'"
+
+dict["sur_name: "] = "das"
+# if we want tp assign a new key and a new keyvalue we can use   "dict["newkey"] = 'new keyvalue'"
+
 print(type(dict))
 print(len(dict))
 print(dict)
