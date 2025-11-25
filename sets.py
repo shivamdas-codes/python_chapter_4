d883ed9cb0b1520d8e37e6f845c9dfc3139c30a6
shivam <shivam984861@gmail.com>
2025-11-25 20:11:06 +0530

    new changes

--- sets.py
+++ sets.py
@@ -31,6 +31,9 @@ collection.add(3)
 collection.add(3)
 collection.add(4)
 # if we want to add any duplicate value, it will always take only one of that duplicate
+collection.add("hello world") # string
+collection.add(("hello", "welcome", "shivam das")) # tuple
+# collection.add([1,2,3]).........we cannot add list inside a set as an element
 print(collection)
 
 set0 = {1,3,5} # the methods not only applicable for the empty set for also applicable to the set which is already assigned by any values.
@@ -42,3 +45,8 @@ print(set0)
 
 
 # (2). remove()
+collection1 = set()
+collection1.add(1)
+collection1.add(2)
+collection1.remove(2) # this method is use to remove the value inside the set
+print(collection1)
