46742963b2a3c4820752351be40e4c642c7e2dc7
shivam <shivam984861@gmail.com>
2025-11-25 19:12:06 +0530

    new changes

---
+++ sets.py
@@ -0,0 +1,17 @@
+set = {1,3,5}
+print(type(set)) # type of the set
+print(set)
+# the above code represents the set.
+
+
+set1 = {1,2,3,"hello", "world"} #we can give any value inside set, like int(), str(), float(), boolean() and tuple() because they are immuatble. we cant use list[] and dictionary{} because thay are mutable
+print(len(set1)) # lenght of the set
+print(set)
+
+
+set2 = {1,1,2,3,4,3, "hello", "world", "hello"}
+print(len(set2)) #the lenght also ignore the duplicate elements and print the unique elements
+# though we know that sets are unorder and each element in the set are unique, it doesn't print any duplicate and alwayes takes the first element. 
+print(set2)
+
+
