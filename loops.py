16e19a06b44da2f1c0d749a3466e4670f32cb951
shivam <shivam984861@gmail.com>
2025-12-01 22:29:50 +0530

    new changes

--- loops.py
+++ loops.py
@@ -1,40 +1,40 @@
-# # while True:
-# #     print("hello")  
-# """in this case the loop never stop at any point because the givenh value is always 'true'
-# if we want to stop loop at any point then the value should contain false at any point."""
+# while True:
+#     print("hello")  
+"""in this case the loop never stop at any point because the givenh value is always 'true'
+if we want to stop loop at any point then the value should contain false at any point."""
 
 
-# i = 1       # variable/iterator 'i' is assigned by 1
-# while i <= 5:   #loop performed till <=5
-#     print("hello")
-#     i += 1      #every time the loop takes place it will increase by "1" till 5
-#     print(i)    #this indicates how many times the loop has occured
+i = 1       # variable/iterator 'i' is assigned by 1
+while i <= 5:   #loop performed till <=5
+    print("hello")
+    i += 1      #every time the loop takes place it will increase by "1" till 5
+    print(i)    #this indicates how many times the loop has occured
 
 
-# j = 1
-# while j <= 100:
-#     print("hello world", j) # the 'j' represent that how many times the hello world will print
-#     j += 1
-#     print(j)    # this represent how many times the loops has occured
-
-
-
-# #DOUBT PROBLEM:
-# students_marks = 10
-# i = students_marks # we can able to change the variable to the new variable name. 
-# while i <= 48:
-#     print("need to improve", i)
-#     i += 2
-# students_marks = i # changed to the original name.
-# del i   # 'i' was deleted and no longer used in this code it is for temporary purpose.
-# print(students_marks)
+j = 1
+while j <= 100:
+    print("hello world", j) # the 'j' represent that how many times the hello world will print
+    j += 1
+    print(j)    # this represent how many times the loops has occured
 
 
 
-# x = [True, True, (True+1)*4]
-# y = False*3 + 1
-# for i in range(y + sum(x) - 9):
-#     print(sum(x)+i)
+#DOUBT PROBLEM:
+students_marks = 10
+i = students_marks # we can able to change the variable to the new variable name. 
+while i <= 48:
+    print("need to improve", i)
+    i += 2
+students_marks = i # changed to the original name.
+del i   # 'i' was deleted and no longer used in this code it is for temporary purpose.
+print(students_marks)
+
+
+
+x = [True, True, (True+1)*4]
+y = False*3 + 1
+for i in range(y + sum(x) - 9):
+    print(sum(x)+i)
 
 
 i = 1
